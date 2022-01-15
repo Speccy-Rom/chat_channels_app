7c956f288ddd69a8655ee3ae26ea615f5c9de8c5
speccy-rom <speccy.rom@gmail.com>
2022-01-15 13:26:32 +0300

    add: Channels Consumer

---
+++ chat/consumers.py
@@ -0,0 +1,57 @@
+import json
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+from .models import Room
+
+
+class ChatConsumer(WebsocketConsumer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.room_name = None
+        self.room_group_name = None
+        self.room = None
+
+    def connect(self):
+        """Внутри connect()мы позвонили accept(), чтобы принять соединение.
+         После этого мы добавили пользователя в группу слоев канала."""
+
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'chat_{self.room_name}'
+        self.room = Room.objects.get(name=self.room_name)
+
+        # connection has to be accepted
+        self.accept()
+
+        # join the room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name,
+        )
+
+    def disconnect(self, close_code):
+        """Внутри disconnect()мы удалили пользователя из группы слоев канала."""
+
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name,
+        )
+
+    def receive(self, text_data=None, bytes_data=None):
+        """Внутри receive()мы разобрали данные в JSON и извлекли файл message.
+         Затем мы перенаправили messageиспользование group_sendв chat_message."""
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        # send chat message event to the room
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
+    def chat_message(self, event):
+        self.send(text_data=json.dumps(event))
