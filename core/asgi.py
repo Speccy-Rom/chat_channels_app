1ca1d5e5edef1ace816e2b614e5a851ad1f3a1bb
speccy-rom <speccy.rom@gmail.com>
2022-01-17 22:10:58 +0300

    added: authentication management called AuthMiddlewareStack

--- core/asgi.py
+++ core/asgi.py
@@ -1,5 +1,6 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
@@ -10,8 +11,10 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = ProtocolTypeRouter(
     {
         'http': get_asgi_application(),
-        'websocket': URLRouter(
-            chat.routing.websocket_urlpatterns
-        ),
+        'websocket': AuthMiddlewareStack(  # new
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
+        ),  # new
     }
 )
